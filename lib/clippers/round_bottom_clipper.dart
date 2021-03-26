659f883004c00056ab5741701261b9f92d694602
Jpec57 <jean.paul.bella@hotmail.fr>
2021-03-26 14:00:52 +0100

    todo

---
+++ lib/clippers/round_bottom_clipper.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class RoundBottomClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    double highestY = size.height * 0.3;
+    var startPoint = Offset(0, size.height * 0.25);
+    var controlPoint = Offset(size.width / 30, size.height * 0.275);
+    var endPoint = Offset(size.width / 10, highestY);
+
+    Path path = new Path();
+    path.lineTo(0, 0);
+    path.lineTo(startPoint.dx, startPoint.dy);
+    path.quadraticBezierTo(
+        controlPoint.dx, controlPoint.dy, endPoint.dx, endPoint.dy);
+    path.lineTo(size.width, highestY);
+    path.lineTo(size.width, 0);
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
+}
